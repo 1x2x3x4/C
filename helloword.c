81a2179adead95360c3f31e16f1f9cc7d0a9b1ec
Hao <2368144523@qq.com>
2025-09-28 21:41:33 +0800

    第一次上传C语言文件
    hello world

---
+++ helloword.c
@@ -0,0 +1,17 @@
+//#include<stdio.h>
+////include 包含 stdio.h stdi standard io input输入
+////output输出 h head头文件 一种文件格式
+//
+//int main(void)
+//{
+//	printf("hello World\n");
+//	//print 打印 f format 格式 格式化字符串 \n代表换行
+//	return 0;
+//}
+
+#include <stdio.h>
+int main(void)
+{
+	printf("Hello World\n");
+	return 0;
+}
