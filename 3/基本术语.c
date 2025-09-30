8928bfaf0c2c400a2bf5709cdb011d6a6aa8260a
Hao <2368144523@qq.com>
2025-09-30 21:34:01 +0800

    基本术语：C

---
+++ 3/基本术语.c
@@ -0,0 +1,22 @@
+#include <stdio.h>	//预处理器指令
+
+int main()	// 入口
+{
+	//1.declaration 声明
+	int number;
+
+	//2.definition	定义
+	int number_2 = 10;
+
+	//3.assignment	赋值
+	number = 25;
+
+	//4.null statement	空语句
+	;
+
+	while (1) {
+		;
+	}
+	while (1) ;
+
+}
