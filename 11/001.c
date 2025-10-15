f3f4c9faf5f96141bfea70c736cd851f48dc3ae6
Hao <2368144523@qq.com>
2025-10-15 23:47:02 +0800

    引入stdint.h库，初始化int类型用法（新）

--- 11/001.c
+++ 11/001.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdint.h>
+
 
 int main(void) {
 
-	__int32 number = 1000;
+	/*__int32 number = 1000;
 
 	long number_1 = 100;
 
@@ -18,7 +20,31 @@ int main(void) {
 
 	printf("%lld\n", number_3);
 
-	printf("%llu\n", number_4);
+	printf("%llu\n", number_4);*/
+
+	int16_t myint16 = 32767;
+
+	uint16_t myUint16 = 65535U;
+
+	int32_t myint32 = 2147483647;
+
+	uint32_t myUint32 = 4294967295U;
+
+	int64_t myint64 = 9223372036854775807LL;
+
+	uint64_t myUint64 = 18446744073709551615ULL;
+
+	printf("size of int16_t : %zu\n", sizeof(myint16));
+
+	printf("size of uint16_t : %zu\n", sizeof(myUint16));
+
+	printf("size of int32_t : %zu\n", sizeof(myint32));
+
+	printf("size of uint32_t : %zu\n", sizeof(myUint32));
+
+	printf("size of int64_t : %zu\n", sizeof(myint64));
+
+	printf("size of uint64_t : %zu\n", sizeof(myUint64));
 
 
 	return 0;
